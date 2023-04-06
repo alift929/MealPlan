585721e820ef873c1100502e2c9488b77d486778
Alif T <alift929@gmail.com>
2023-04-06 10:37:48 +0700

    Initial commit

---
+++ src/main/java/com/mealgroup/mealplan/MealPlan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mealgroup.mealplan;
+
+/**
+ *
+ * @author alift
+ */
+public class MealPlan {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
